d0dccf5c18eaf8a0bc2120d387d57c29cf9c8a83
vinod9030 <vinod@promactinfo.com>
2022-09-26 13:36:34 +0530

    ecs-1

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.20.1"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-south-1"
+  access_key = "add-your-key"
+  secret_key = "add-your-access_key"
+  region     = var.aws_region
+  #if you are running from AWS ec2 linux instance please use bellow credentials section
+  #shared_credentials_file = "$HOME/.aws/credentials"
+  #profile = "default"
 }
