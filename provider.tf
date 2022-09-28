1273dea661c2da529e8e60504e1c907ce57ed31b
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-09-28 13:32:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  access_key = "add-your-key"
-  secret_key = "add-your-access_key"
-  region     = var.aws_region
+  region = "us-east-1"
+}
+
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
   #profile = "default"
