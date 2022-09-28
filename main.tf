13472fa1358350d22b13ef3a4c192f10d4f9cc3a
vinod9030 <vinod@promactinfo.com>
2022-09-28 13:20:17 +0530

    ecs-2

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "visi-pro" {
+  cidr_block = "10.0.0.0/16"
+}
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
