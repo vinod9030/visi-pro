ba1392f19f2171e9aaefceb3aee021ff29ac0eb4
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-09-28 13:28:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,6 @@ resource "aws_vpc" "visi-pro" {
   cidr_block = "10.0.0.0/16"
 }
 
-provider "" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
-
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
