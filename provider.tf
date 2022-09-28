186362e75aaee058da5b195a27ca196d800b30a8
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-09-28 13:33:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
-}
