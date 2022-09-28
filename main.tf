a63d8a29967d963d7b7d2f4f467582b01fc07b91
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-09-28 13:56:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
     tags = {
-        Name = "${var.vpc_name}"
+        Name = "${var.vpc_visi-pro}"
     }
 }
 
