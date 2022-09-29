e46f5a732d2da99d009ad2f64c10609473d26500
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-09-29 13:31:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_security_group" "allow_all" {
 
 resource "aws_instance" "web-1" {
     #ami = "${data.aws_ami.my_ami.id}"
-    ami = "ami-0d857ff0f5fc4e03b"
+    ami = "ami-08c40ec9ead489470"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
     key_name = "LaptopKey"
