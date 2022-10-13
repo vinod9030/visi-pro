6a9bcdb2004c27a5cb0d3f6468bda00d1086d2ac
vinod9030 <vinod@promactinfo.com>
2022-10-13 15:22:10 +0530

    ecs-2

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "add-your-key"
-  secret_key = "add-your-access_key"
+  access_key = "APKATD7PANJR3GE2XQ2U"
+  secret_key = "7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
