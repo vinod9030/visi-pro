8d351f1c7b9706adab84460977284d7dcf56f571
vinod9030 <vinod@promactinfo.com>
2022-10-13 15:16:54 +0530

    ecs-2

--- variables.tf
+++ variables.tf
@@ -4,10 +4,10 @@ variable "aws_region"{}
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+      us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+		  us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		  us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		  us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
     }
 }
 variable "vpc_cidr"{}
