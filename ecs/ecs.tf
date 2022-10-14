f9e7f7f9364ec45f5b169ff773f289bcf4269e2d
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-14 19:28:38 +0530

    Update ecs.tf

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "test-cluster" {
-  name = "myapp-cluster"
+  name = "visi-proapp-cluster"
 }
 
 data "template_file" "testapp" {
@@ -14,8 +14,8 @@ data "template_file" "testapp" {
   }
 }
 
-resource "aws_ecs_task_definition" "test-def" {
-  family                   = "testapp-task"
+resource "aws_ecs_task_definition" "visi-pro-def" {
+  family                   = "visi-proapp-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -24,24 +24,24 @@ resource "aws_ecs_task_definition" "test-def" {
   container_definitions    = data.template_file.testapp.rendered
 }
 
-resource "aws_ecs_service" "test-service" {
-  name            = "testapp-service"
-  cluster         = aws_ecs_cluster.test-cluster.id
-  task_definition = aws_ecs_task_definition.test-def.arn
+resource "aws_ecs_service" "visi-pro-service" {
+  name            = "visi-proapp-service"
+  cluster         = aws_ecs_cluster.visi-pro-cluster.id
+  task_definition = aws_ecs_task_definition.visi-pro-def.arn
   desired_count   = var.app_count
   launch_type     = "FARGATE"
 
   network_configuration {
     security_groups  = [aws_security_group.ecs_sg.id]
-    subnets          = aws_subnet.private.*.id
+    subnets          = aws_subnet.private.subnet-04ed3aeb3e95cdd42
     assign_public_ip = true
   }
 
   load_balancer {
     target_group_arn = aws_alb_target_group.myapp-tg.arn
-    container_name   = "testapp"
+    container_name   = "visi-proapp"
     container_port   = var.app_port
   }
 
-  depends_on = [aws_alb_listener.testapp, aws_iam_role_policy_attachment.ecs_task_execution_role]
+  depends_on = [aws_alb_listener.visi-proapp, aws_iam_role_policy_attachment.ecs_task_execution_role]
 }
