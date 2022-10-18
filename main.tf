2e1736b832be5babf94cdcf3d3e9c6b40abe48cf
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-18 17:19:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ terraform {
 
 # Configure the AWS Provider
  
-{
-provider "aws"
+
+provider "aws" {
     access_key = "{APKATD7PANJR3GE2XQ2U}"
     secret_key = "{7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj}"
     region = "{us-east-1}"
-}
+
 
 resource "aws_vpc" "default" {
     cidr_block ="172.31.0.0/20"
