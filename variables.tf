71d74459d2124192d27617054c5e77fc67ef2415
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-18 16:24:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,42 @@
-variable "app_count" {
-  type = number
-  default = 1
+variable "aws" {
+aws_access_key = "{APKATD7PANJR3GE2XQ2U}"
+aws_secret_key = "{7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj}" 
+aws_region = "{us-east-1}" 
 }
-
-data "aws_availability_zones" "available_zones" {
-  state = "available"
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+      us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+		  us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		  us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		  us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+    }
+}
+vpc_cidr = "{172.31.0.0/16}"
+public_subnet1_cidr = "{172.31.80.0/20}"
+public_subnet2_cidr = "{172.31.16.0/20}"
+public_subnet3_cidr = "{172.31.32.0/20}"
+private_subnet_cidr = "{172.31.48.0/20}"
+vpc_name = "{visi-pro}"
+IGW_name = "{visi-proigw}"
+public_subnet1_name = "{visi-pro/Public}"
+public_subnet2_name = "{visi-pro/Public}"
+public_subnet3_name = "{visi-pro/Public}"
+private_subnet_name = "{visi-pro/Private}"
+Main_Routing_Table = "{visi-pro}"
+key_name = "{visi-pro}"
+environment = "{dev}"
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  
+  default = {
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
 }
