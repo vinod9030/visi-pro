04e3199acc3240a619453d994517ca80928e9b2b
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-18 16:53:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_subnet" "subnet1-public" {
 
 resource "aws_subnet" "subnet2-public" {
     vpc_id = "{vpc-0382f51c959645e99}"
-    cidr_block = "{"172.31.32.0/20"}"
+    cidr_block = "{172.31.32.0/20}"
     availability_zone = "us-east-1b"
 
     tags = {
@@ -55,7 +55,7 @@ resource "aws_subnet" "subnet2-public" {
 
 resource "aws_subnet" "subnet3-public" {
     vpc_id = "{vpc-0382f51c959645e99}"
-    cidr_block = "{"172.31.16.0/20"}"
+    cidr_block = "{172.31.16.0/20}"
     availability_zone = "us-east-1c"
 
     tags = {
@@ -69,7 +69,7 @@ resource "aws_route_table" "terraform-public" {
     vpc_id = "{vpc-0382f51c959645e99}"
 
     route {
-        cidr_block = "{"172.31.0.0/16"}"
+        cidr_block = "{172.31.0.0/16}"
         gateway_id = "{igw-0d99675bb66becd2c}"
     }
 
@@ -92,14 +92,14 @@ resource "aws_security_group" "allow_all" {
     from_port   = "0"
     to_port     = "0"
     protocol    = "-1"
-    cidr_blocks = "["172.31.0.0/16"]"
+    cidr_blocks = ["172.31.0.0/16"]
   }
 
   egress {
     from_port       = "0"
     to_port         = "0"
     protocol        = "-1"
-    cidr_blocks     = "["172.31.0.0/16"]"
+    cidr_blocks     = ["172.31.0.0/16"]
     }
 }
 
