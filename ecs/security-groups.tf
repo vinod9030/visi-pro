980f33d721ea5cb6ec1b5dcff01db8b563db78e4
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-18 16:13:20 +0530

    Update security-groups.tf

--- ecs/security-groups.tf
+++ ecs/security-groups.tf
@@ -4,20 +4,20 @@
 resource "aws_security_group" "visi-pro" {
   name        = "visi-proapp-load-balancer-security-group"
   description = "controls access to the ALB"
-  vpc_id      = aws_vpc.visi-pro-vpc.vpc-0e4ba9e87082d56aa
+  vpc_id      = "aws_vpc.visi-pro-vpc.vpc-0382f51c959645e99"
 
   ingress {
     protocol    = "tcp"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    cidr_blocks = ["10.0.0.0/24"]
+    from_port   = "var.app_port"
+    to_port     = "var.app_port"
+    cidr_blocks = ["172.31.0.0/16"]
   }
 
   egress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
 
@@ -25,19 +25,19 @@ resource "aws_security_group" "visi-pro" {
 resource "aws_security_group" "visi-pro" {
   name        = "visi-proapp-ecs-tasks-security-group"
   description = "allow inbound access from the ALB only"
-  vpc_id      = aws_vpc.visi-pro-vpc.vpc-0382f51c959645e99
+  vpc_id      ="aws_vpc.visi-pro-vpc.vpc-0382f51c959645e99"
 
   ingress {
     protocol        = "tcp"
-    from_port       = var.app_port
-    to_port         = var.app_port
-    security_groups = [aws_security_group.alb-sg.sg-0cfcad0bc79947e74]
+    from_port       = "var.app_port"
+    to_port         = "var.app_port"
+    security_groups = "[aws_security_group.alb-sg.sg-0551d0f7731151aa3]"
   }
 
   egress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
