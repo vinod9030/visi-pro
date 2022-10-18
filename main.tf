e892b448b8b095f35646325899a2f2c9965571e7
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-18 17:57:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 # Configure the AWS Provider
  
 
-provider "aws" 
+provider "aws" {
     access_key = "{APKATD7PANJR3GE2XQ2U}"
     secret_key = "{7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj}"
     region = "{us-east-1}"
