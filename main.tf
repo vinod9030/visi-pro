90d9300be065a5222668d47ec4b5bc4c58f02785
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-19 14:34:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
  
 
 provider "aws" {
-    aws_access_key = "APKATD7PANJR3GE2XQ2U"
-    aws_secret_key = "7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj"
-    aws_region = "us-east-1"
+    access_key = "APKATD7PANJR3GE2XQ2U"
+    secret_key = "7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj"
+    region = "us-east-1"
     
 }
 
