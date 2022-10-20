2a0955c16084fcdd83dd6a7d007d214171b3fba9
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-20 13:26:15 +0530

    Update provider.tf

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "APKATD7PANJR3GE2XQ2U"
   secret_key = "7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj"
-  region     = "var.aws_region"
+  region     = "us-east-1"
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
   #profile = "default"
