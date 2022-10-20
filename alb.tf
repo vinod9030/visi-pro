c0ca06c32553c45b09399f4ff1aac805e07665b9
vinod9030 <vinod@promactinfo.com>
2022-10-20 18:41:17 +0530

    ecs-2

--- alb.tf
+++ alb.tf
@@ -1,7 +1,7 @@
 resource "aws_lb" "default" {
   name            = "visi-pro-lb"
-  subnets         =  "aws_subnet.public.04ed3aeb3e95cdd42"
-  security_groups = "sg-0cfcad0bc79947e74"
+   subnets         = "aws_subnet.public.*.id"
+  security_groups = "aws_security_group.lb.id"
 }
 
 
@@ -10,21 +10,21 @@ resource "aws_lb_target_group" "hello_world" {
   name        = "example-target-group"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = "vpc-0e4ba9e87082d56aa"
+  vpc_id      = "aws_vpc.default.id"
   target_type = "ip"
 }
 
 
 
 resource "aws_lb_listener" "hello_world" {
-  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:214712740451:loadbalancer/app/visi-pro/d3b1a8885700f386"
+  load_balancer_arn = "aws_lb.default.id"
   port              = "80"
   protocol          = "HTTP"
 
 
 
  default_action {
-    target_group_arn = "hello_world.arn:aws:elasticloadbalancing:us-east-1:214712740451:targetgroup/visi-pro/e18badd3c41f4b80id"
+    target_group_arn = "aws_lb_target_group.hello_world.id"
     type             = "forward"
   }
 }
