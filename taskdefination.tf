8dd5e9de35599fa00ad07eee330aaf55ac1f91b1
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-20 17:40:02 +0530

    Update taskdefination.tf

--- taskdefination.tf
+++ taskdefination.tf
@@ -27,13 +27,13 @@ DEFINITION
 
 resource "aws_security_group" "hello_world_task" {
   name        = "example-task-security-group"
-  vpc_id      = "aws_vpc.default.id"
+  vpc_id      = "aws_vpc.default.vpc-0e4ba9e87082d56aa"
 
   ingress {
     protocol        = "tcp"
     from_port       = 3000
     to_port         = 3000
-    security_groups = "aws_security_group.lb.id"
+    security_groups = "aws_security_group.lb.sg-0cfcad0bc79947e74"
   }
 
   egress {
