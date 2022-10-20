8431c6cc401fa08eb1d8cadc467b94737d0c50b0
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-20 13:30:02 +0530

    Update vpc.tf

--- ecs/vpc.tf
+++ ecs/vpc.tf
@@ -1,70 +1,78 @@
-# network.tf
-
-resource "aws_vpc" "test-vpc" {
-  cidr_block = "172.31.0.0/16"
-}
-
-# Fetch AZs in the current region
-data "aws_availability_zones" "available" {
-}
-
-
-# Create var.az_count private subnets, each in a different AZ
-resource "aws_subnet" "private" {
-  count             = var.az_count
-  cidr_block        = cidrsubnet(aws_vpc.test-vpc.172.31.48.0/20, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = aws_vpc.test-vpc.vpc-0382f51c959645e99
-}
-
-# Create var.az_count public subnets, each in a different AZ
-resource "aws_subnet" "public" {
-  count                   = var.az_count
-  cidr_block              = cidrsubnet(aws_vpc.test-vpc.172.31.80.0/20, 8, var.az_count + count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  vpc_id                  = aws_vpc.test-vpc.vpc-0382f51c959645e99
-  map_public_ip_on_launch = true
-}
-
-# Internet Gateway for the public subnet
-resource "aws_internet_gateway" "visi-pro-igw" {
-  vpc_id = aws_vpc.test-vpc.vpc-0382f51c959645e99
-}
-
-# Route the public subnet traffic through the IGW
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.test-vpc.main_route_table_rtb-07d1bd9f86996008e
-  destination_cidr_block = "172.31.0.0/16"
-  gateway_id             = aws_internet_gateway.test-igw.igw-0d99675bb66becd2c
-}
-
-# Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
-resource "aws_eip" "visi-pro-eip" {
-  count      = var.az_count
-  vpc        = true
-  depends_on = [aws_internet_gateway.test-igw-0d99675bb66becd2c]
-}
-
-resource "aws_nat_gateway" "visi-pro-natgw" {
-  count         = var.az_count
-  subnet_id     = element(aws_subnet.public.subnet-06d9db5a1de59d5c7, count.index)
-  allocation_id = element(aws_eip.test-eip.eipalloc-08f57de18ff4c9bdb, count.index)
-}
-
-# Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
-resource "aws_route_table" "private" {
-  count  = var.az_count
-  vpc_id = aws_vpc.test-vpc.vpc-0382f51c959645e99
-
-  route {
-    cidr_block     = "172.31.0.0/16"
-    nat_gateway_id = element(igw-0d99675bb66becd2c, count.index)
+resource "aws_vpc" "default" {
+  cidr_block = "10.32.0.0/16"
+  tags = {
+    Name = "Taskvpc"
   }
 }
 
-# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_subnet" "public" {
+  count                   = 2
+  cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
+  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
+  vpc_id                  = aws_vpc.default.id
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+  vpc_id            = aws_vpc.default.id
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
+
+resource "aws_eip" "gateway" {
+  count      = 2
+  vpc        = true
+  depends_on = [aws_internet_gateway.gateway]
+}
+
+resource "aws_nat_gateway" "gateway" {
+  count         = 2
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.gateway.*.id, count.index)
+}
+
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
+  }
+}
+
 resource "aws_route_table_association" "private" {
-  count          = var.az_count
-  subnet_id      = element(aws_subnet.private.subnet-054d60902fc47a8b3, count.index)
-  route_table_id = element(aws_route_table.private.rtb-07d1bd9f86996008e, count.index)
+  count          = 2
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_security_group" "lb" {
+  name        = "example-alb-security-group"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
