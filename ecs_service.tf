24a386ba100acdba60ce7abf65632282f3ddb4be
vinod9030 <vinod@promactinfo.com>
2022-10-21 03:33:21 +0530

    ecs-2

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,20 +1,20 @@
-resource "aws_ecs_service" "hello_world" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.hello_world.arn
-  desired_count   = var.app_count
+resource "aws_ecs_service" "visi-pro" {
+  name            = "visi-pro-service"
+  cluster         = "aws_ecs_cluster.main. arn:aws:ecs:us-east-1:214712740451:cluster/visi-pro"
+  task_definition = "aws_ecs_task_definition.visi-pro. arn:aws:ecs:us-east-1:214712740451:cluster/visi-pro"
+  desired_count   = "var.app_count"
   launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups = [aws_security_group.hello_world_task.id]
-    subnets         = aws_subnet.private.*.id
+    security_groups = "aws_security_group.visi-pro_task.sg-0cfcad0bc79947e74"
+    subnets         = "aws_subnet.private.subnet-0c28e449e25db94b0"
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.hello_world.id
-    container_name   = "hello-world-app"
+    target_group_arn = "aws_lb_target_group.visi-pro.arn:aws:elasticloadbalancing:us-east-1:214712740451:targetgroup/visi-pro/e18badd3c41f4b80"
+    container_name   = "visi-pro-app"
     container_port   = 3000
   }
 
-  depends_on = [aws_lb_listener.hello_world]
+  depends_on = "[aws_lb_listener.visi-pro]"
 }
