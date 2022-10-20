75b2057799fa665375c7ccdf322c765d780e61c2
vinod9030 <108118167+vinod9030@users.noreply.github.com>
2022-10-20 13:51:38 +0530

    Delete provider.tf

--- ecs/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  access_key = "APKATD7PANJR3GE2XQ2U"
-  secret_key = "7k7BcYF4aDTorCxp7q/3hOHomi0PEmH4wKR5boCj"
-  region     = "us-east-1"
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
-}
